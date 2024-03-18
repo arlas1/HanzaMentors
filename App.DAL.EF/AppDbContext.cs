526e6f7feb20b49ead65a6f715643d24ec92dff3
Artur <arlssi@ttu.ee>
2024-03-18 16:40:49 +0200

    docs

--- App.DAL.EF/AppDbContext.cs
+++ App.DAL.EF/AppDbContext.cs
@@ -11,29 +11,30 @@ public class AppDbContext : IdentityDbContext<AppUser, AppRole, Guid, IdentityUs
 
 {
     public DbSet<DocumentSample> DocumentSamples { get; set; } = default!;
-    
+
     public DbSet<Intern> Interns { get; set; } = default!;
     public DbSet<InternMentorship> InternMentorships { get; set; } = default!;
     public DbSet<InternSupervisor> InternSupervisors { get; set; } = default!;
     public DbSet<InternMentorshipDocument> InternMentorshipDocuments { get; set; } = default!;
     public DbSet<InternMentorshipUntilDate> InternMentorshipUntilDates { get; set; } = default!;
-    
+
     public DbSet<Employee> Employees { get; set; } = default!;
     public DbSet<EmployeeMentorship> EmployeeMentorships { get; set; } = default!;
     public DbSet<FactorySupervisor> FactorySupervisors { get; set; } = default!;
     public DbSet<EmployeeMentorshipDocument> EmployeeMentorshipDocuments { get; set; } = default!;
     public DbSet<EmployeeMentorshipUntilDate> EmployeeMentorshipUntilDates { get; set; } = default!;
-    
+
     public DbSet<Mentor> Mentors { get; set; } = default!;
     public DbSet<MenteeSickLeave> MenteeSickLeaves { get; set; } = default!;
     public DbSet<SickLeaveType> SickLeaveTypes { get; set; } = default!;
 
-    
+
     public AppDbContext(DbContextOptions options) : base(options)
     {
     }
+}
 
-    //
+//
     // public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
     // {
     //     foreach (var entity in ChangeTracker.Entries().Where(e => e.State != EntityState.Deleted))
@@ -49,4 +50,3 @@ public class AppDbContext : IdentityDbContext<AppUser, AppRole, Guid, IdentityUs
     //
     //     return base.SaveChangesAsync(cancellationToken);
     // }
-}
